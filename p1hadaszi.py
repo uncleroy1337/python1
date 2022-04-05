a1a3a05b387d9c3915e3604837cb36ae5ff3f1bb
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-05 09:03:37 +0200

    Create p1hadaszi.py

---
+++ p1hadaszi.py
@@ -0,0 +1,19 @@
+"""
+1. Feladat
+Készíts egy rövid programot, amely 1 és 3 között generál véletlenszámot, majd összehasonlítja ezt a felhasználó által megadott, szintén ebbe a tartományba eső számmal! Az összehasonlítás eredményéről tájékoztassa a felhasználót!
+"""
+
+import random
+
+lista = [1, 2, 3]
+
+randomszam = int(input("Írj be egy számot: "))
+szam = random.choice(lista)
+print(szam)
+
+if szam > randomszam:
+  print("A beírt szám kisebb!")
+if szam < randomszam:
+  print("A beírt szám nagyobb!")
+if szam == randomszam:
+  print("A beírt szám egyenlő!")
