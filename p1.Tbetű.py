3af27a391051917150a81d713cd8c15994091b51
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-05 11:34:47 +0200

    Update p1.Tbetű.py

--- p1.Tbetű.py
+++ p1.Tbetű.py
@@ -4,11 +4,8 @@ A  ['ajtó','tojás','Ottó','Tamás', 'tép','Tesla','alma','python'] lista ele
 Az előző programot alakítsd át úgy, hogy a feltételeknek megfelelő szavak kerüljenek rögzítésre egy másik listában is, és ez utóbbi listát írja ki a program a képernyőre!
 '''
 lista1=[]
-lista2=[]
 lista = ['ajtó','tojás','Ottó','Tamás', 'tép','Tesla','alma','python']
 
 for i in lista:
     if i[0] == 't' or i[0] == 'T':
         lista1.append(i)
-        lista2.append(i)
-print(lista2)
