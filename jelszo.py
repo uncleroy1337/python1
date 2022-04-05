b90f84c8b4e2f24d66778d1178edc8732f658f73
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2022-04-05 11:13:18 +0200

    Update jelszo.py

--- jelszo.py
+++ jelszo.py
@@ -1,4 +1,7 @@
- ## Írjon programot jelszo.py néven, amely azt vizsgálja, hogy egy felhasználó helyesen adja-e meg a jelszavát! A program addig kérdezi újra a felhasználónév-jelszó párost, amíg a felhasználó mindkettőt hibátlanul meg nem adja. A program egyetlen felhasználó (bori99) jelszavát (Szivecske<3) ismeri, csak ezt a párost fogadja el helyesként. Mind a sikertelen, mind a sikeres bejelentkezési kísérletekről üzenetet ír a képernyőre.
+ ## Írjon programot jelszo.py néven, amely azt vizsgálja, hogy egy felhasználó helyesen adja-e meg a jelszavát! 
+ ##A program addig kérdezi újra a felhasználónév-jelszó párost, amíg a felhasználó mindkettőt hibátlanul meg nem adja. 
+ ##A program egyetlen felhasználó (sis) jelszavát (1234) ismeri, csak ezt a párost fogadja el helyesként. 
+ ##Mind a sikertelen, mind a sikeres bejelentkezési kísérletekről üzenetet ír a képernyőre.
 
 felhasznalonev = input('Kerem a nevet: ')
 password = input('Kerem a jelszot: ')
