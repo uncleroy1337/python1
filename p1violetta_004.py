9cf97de57e4e2ea60657fed1bc1d564ca57380a1
violetta2413 <71691747+violetta2413@users.noreply.github.com>
2022-04-05 10:11:00 +0200

    Create p1violetta_004.py

---
+++ p1violetta_004.py
@@ -0,0 +1,9 @@
+'''
+Készíts egy programot, ami bekér egy számot a felhasználótól 
+és kiírja a képernyőre az utolsó számjegyét!
+'''
+
+#megoldás
+
+szam = int(input("Adj meg egy számot:"))
+print(szam % 10)
