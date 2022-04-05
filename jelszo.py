715dc5f0ad6e52e393cec8330adcd6116faf98f0
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2022-04-05 09:10:04 +0200

    Add files via upload

---
+++ jelszo.py
@@ -0,0 +1,8 @@
+ ## Írjon programot jelszo.py néven, amely azt vizsgálja, hogy egy felhasználó helyesen adja-e meg a jelszavát! A program addig kérdezi újra a felhasználónév-jelszó párost, amíg a felhasználó mindkettőt hibátlanul meg nem adja. A program egyetlen felhasználó (bori99) jelszavát (Szivecske<3) ismeri, csak ezt a párost fogadja el helyesként. Mind a sikertelen, mind a sikeres bejelentkezési kísérletekről üzenetet ír a képernyőre.
+
+felhasznalonev = input('Kerem a nevet: ')
+password = input('Kerem a jelszot: ')
+if felhasznalonev == "sis" and password == "1234":
+  print("Üdvözlöm!")
+else:
+  print('Sikertelen bejelentkezes')
