b153909da2bbccf81339e78c998b054b89b7bf7d
violetta2413 <71691747+violetta2413@users.noreply.github.com>
2022-04-05 09:21:07 +0200

    Create p1violetta_002.py

---
+++ p1violetta_002.py
@@ -0,0 +1,14 @@
+''' Készíts egy programot, amely [1;10] intervallumon generál 5 darab véletlen egész számot és ezeket tárolja el egy listában! 
+    A program számolja össze, és képernyőre írja ki a listában tárolt páros számok számát, valamint a lista elemeit! '''
+
+#megoldás
+
+import random
+
+lista = [random.randint(1,10) for i in range(5)]
+
+paroslista = [i for i in lista if i % 2 == 0]
+
+print(f"A páros számok száma:{len(paroslista)}")
+print(lista)
+print(paroslista)
