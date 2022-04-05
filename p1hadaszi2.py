55130861d9821d8fa8838f1cf29607e2f4a10aa7
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-05 09:12:11 +0200

    Create p1hadaszi2.py

---
+++ p1hadaszi2.py
@@ -0,0 +1,15 @@
+'''
+Készíts egy programot, amely a felhasználótól számokat kér be mindaddig, amíg az csupán ENTER-t nem üt! A számokat tárolja el a program egy listában! Az adatbekérés befejezte után írja ki a program a lista elemeit, a legkisebb és a legnagyobb számot!
+'''
+
+lista = []
+folytatja = True
+while folytatja:
+  bekeres = int(input("Kérek egy számot\t"))
+  lista.append(bekeres)
+  print(lista)
+  if bekeres == "":
+    folytatja = False
+
+print(f"lista elemei: {lista}")
+print(f"legkisebb: {min(lista)} legnagyobb: {max(lista)}")
