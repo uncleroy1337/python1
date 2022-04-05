4a80ab2994e3be6a76613a7fa1223da12dee3934
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-05 09:14:29 +0200

    Update and rename p1hadaszi.py to p1hadaszi.generál.py

--- p1hadaszi.generál.py
+++ p1hadaszi.generál.py
@@ -1,5 +1,4 @@
 """
-1. Feladat
 Készíts egy rövid programot, amely 1 és 3 között generál véletlenszámot, majd összehasonlítja ezt a felhasználó által megadott, szintén ebbe a tartományba eső számmal! Az összehasonlítás eredményéről tájékoztassa a felhasználót!
 """
 
