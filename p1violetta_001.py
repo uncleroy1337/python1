af41f9c8d58f4ef65e3623ca8a5960f84cd7762d
violetta2413 <71691747+violetta2413@users.noreply.github.com>
2022-04-05 09:08:40 +0200

    Create p1violetta_001.py

---
+++ p1violetta_001.py
@@ -0,0 +1,14 @@
+''' A program kérjen be két szót a felhasználótól, majd írja ki, hogy melyik a hosszabb illetve azt is ha a két szó egyenlő! '''
+
+#megoldas
+
+szo1 = input("Adj meg egy szót!")
+szo2 = input("Adj meg egy másik szót!")
+
+if len(szo1) > len(szo2):
+  print(f'A hosszabb szó:{szo1}')
+elif len(szo1) == len(szo2):
+  print(f'A két szó egyforma hosszú.')
+else:
+  len(szo2) > len(szo1)
+  print(f'A hosszabb szó:{szo2}')
