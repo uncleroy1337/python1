7dce360a7b5857f86a85a2a85f357dae4cd3b817
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-04-05 08:51:43 +0200

    a

--- p1arpi_001.py
+++ p1arpi_001.py
@@ -1,1 +1,11 @@
-print('ize')
+''' Írjon egy programot ami kiírja a kettővel osztható számokat 1-től 100-ig majd irassa ki a számok gyökét, és négyzetét'''
+
+#megoldas
+lista = [szam for szam in range(101)if szam %2 == 0]
+print(lista)
+
+for szam in lista:
+    print(szam ** 0.5)
+
+for szam in lista:
+    print(szam ** 2)
