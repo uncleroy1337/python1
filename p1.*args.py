de3f9bdf4f42b1bfdabf062796715b94c8eabc7d
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-05 10:57:10 +0200

    Delete p1.*args.py

--- p1.*args.py
+++
@@ -1,9 +0,0 @@
-'''
-Írj egy programot, amely tartalmaz egy 'osszegzo' nevű függvényt, ami a paraméterként átvett listaelemeket (egész számokat) összeadja és az összegükkel tér vissza! 
-A program tartalmazza a függvény hívását is!
-'''
-
-def osszegzo(*args):
-    return sum([ num for num in args])
-
-print(osszegzo(1,2,3,4,5,98,100))
