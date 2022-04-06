ecfdedfb1ccacd461890931d4ea42ad93eceaa42
ZamArtem <72965392+ZamArtem@users.noreply.github.com>
2022-04-06 14:27:21 +0200

    Update p1.Tbetű.py

--- p1.Tbetű.py
+++ p1.Tbetű.py
@@ -1,11 +1,10 @@
 '''
 A  ['ajtó','tojás','Ottó','Tamás', 'tép','Tesla','alma','python'] lista elemei közül a program kiírja a "t" és "T" betűkkel kezdődőeket!
-########################################################################################################################################
-Az előző programot alakítsd át úgy, hogy a feltételeknek megfelelő szavak kerüljenek rögzítésre egy másik listában is, és ez utóbbi listát írja ki a program a képernyőre!
 '''
-lista1=[]
+t_betu=[]
 lista = ['ajtó','tojás','Ottó','Tamás', 'tép','Tesla','alma','python']
 
 for i in lista:
     if i[0] == 't' or i[0] == 'T':
-        lista1.append(i)
+        t_betu.append(i)
+print(t_betu)
