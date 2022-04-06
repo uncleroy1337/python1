11bfedc5508dc9ef98454b8fcd29cc39701a9ec9
ZamArtem <72965392+ZamArtem@users.noreply.github.com>
2022-04-06 14:17:51 +0200

    Update haromnegy.py

--- haromnegy.py
+++ haromnegy.py
@@ -4,13 +4,13 @@ a szám csak 4-gyel osztható,
 a szám 3-mal és 4-gyel is osztható,
 a szám sem 3-mal, sem 4-gyel nem osztható!"""
 
-x = int(input("Kérek egy pozitív egész számot: "))
+szam = int(input("Kérek egy pozitív egész számot: "))
 
-if x % 3 == 0 and x % 4 ==0:
+if szam % 3 == 0 and szam % 4 ==0:
     print("A szám 3-mal és 4-gyel is osztható.")
-elif x % 3 == 0:
+elif szam % 3 == 0:
     print("A szám csak 3-mal osztható")
-elif x % 4 == 0:
+elif szam % 4 == 0:
     print("A szám csak 4-gyel osztható")
 else:
     print("A szám sem 3-mal, sem 4-gyel nem osztható!")
