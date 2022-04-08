ad5c7ddadf51b12392ad3ec70e2a96f25d5dd707
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2022-04-08 16:21:12 +0200

    Update mindadig_mig_x.py

--- mindadig_mig_x.py
+++ mindadig_mig_x.py
@@ -2,12 +2,12 @@
 Készíts egy programot, amely a felhasználótól számokat kér be mindaddig, amíg az 'X'-et vagy 'x'-et nem üt! A számokat tárolja el a program egy listában! 
 Az adatbekérés befejezte után írja ki a program a lista elemeit, a legkisebb és a legnagyobb számot!
 '''
+
 lista = []
 while True:
         szamok = input("Kérek egy számot: ")
         if szamok == "x" or szamok == "X":
             break
         else:
-            lista.append(szamok)
+            lista.append(int(szamok))
 print(f"Legkissebb: {min(lista)} Legnagyobb: {max(lista)}")
-            
